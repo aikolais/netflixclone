9d632265325b2110ff422ba192f742a760ad2d99
lais.dalpozur <lais.dalpozur@w3k.com.br>
2022-01-23 20:10:27 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
+
 import './App.css';
 
-function App() {
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+
+useEffect(() => {      // QUANDO CARREGAR A TELA VAI EXECUTAR A FUNÇÃO
+  const loadAll = async () => {
+// pegando a lista total
+let list = await Tmdb.getHomeList();
+setMovieList(list);
+
+// pegando o featured
+
+let originals = list.filter(i=>i.slug === 'originals');
+let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+let chosen = originals[0].items.results[randomChosen];
+// console.log(chosen)
+let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+// console.log(chosenInfo);
+setFeatureData(chosenInfo)
+  }
+
+  loadAll();
+}, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+
+     {featureData &&
+     <FeatureMovie item={featureData} />
+     }
+
+<section className="list">
+  {movieList.map((item, key) => (
+      <MovieRow key={key} title={item.title} items={item.items}></MovieRow>
+  ))}
+</section>
     </div>
   );
 }
 
-export default App;
